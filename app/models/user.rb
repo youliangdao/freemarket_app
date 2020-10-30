4afe099912011768e1d6ba2a604378c3090cffbc
youliangdao <youliangdaojing5@gmail.com>
2020-10-30 16:10:12 +0900

    userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
 
   has_many :items
   has_many :purchases
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+    validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
+  end
+
+  validates :email, uniqueness: {case_sensitive: false}
+  validates :encrypted_password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
 end
