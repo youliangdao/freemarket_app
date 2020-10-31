a19d58ca5f8b8a9b1cf6de54367a5416b0fbbb86
youliangdao <youliangdaojing5@gmail.com>
2020-10-31 21:32:31 +0900

    出品機能のバックエンド実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,19 +4,19 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
   end
 
   def create
-    # item = Item.create(item_params)
-    # redirect_to new_item_path and return unless item.valid?
-    # redirect_to root_path
+    @item = Item.create(item_params)
+    render action: :new and return unless @item.valid?
+    redirect_to root_path
   end
 
-  # private
+  private
 
-  # def item_params
-  #   params.require(:item).permit(:image, :title, :detail, :category_id, 
-  #                                :condition_id, :postage_id, :prefecture_id, :delivery_id, :price).merge(user_id: current_user.id)
-  # end
+  def item_params
+    params.require(:item).permit(:image, :title, :detail, :category_id, 
+                                 :condition_id, :postage_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
 end
