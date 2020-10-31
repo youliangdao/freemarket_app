b1e02befeeeba7f9c12484f52e22feab720934ad
youliangdao <youliangdaojing5@gmail.com>
2020-10-31 20:56:21 +0900

    非ログイン時に、ログイン画面へ遷移する機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
   end
 
@@ -7,6 +8,15 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+    # item = Item.create(item_params)
+    # redirect_to new_item_path and return unless item.valid?
+    # redirect_to root_path
   end
+
+  # private
+
+  # def item_params
+  #   params.require(:item).permit(:image, :title, :detail, :category_id, 
+  #                                :condition_id, :postage_id, :prefecture_id, :delivery_id, :price).merge(user_id: current_user.id)
+  # end
 end
