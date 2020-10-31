9a3eb019526010708ad33b5440ce58e4bec0deef
youliangdao <youliangdaojing5@gmail.com>
2020-10-31 13:48:59 +0900

    FactoryBotをテストコードで使えるように導入

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,9 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require "factory_bot"
+require "faker"
+require "gimei"
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -35,7 +37,7 @@ I18n.locale = :en
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  config.include FactoryBot::Syntax::Methods
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
