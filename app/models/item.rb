2da8f78ad996b7f596fb387fef8a6a0026f863a2
youliangdao <youliangdaojing5@gmail.com>
2020-10-31 20:15:39 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one :purchase
+  has_one_attached :image
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
