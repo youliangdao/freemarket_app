caebf7701d6be4ec078694f9657524a913ffbcd9
youliangdao <youliangdaojing5@gmail.com>
2020-11-01 14:27:52 +0900

    コードレビュー　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    @items = Item.all.order(id: 'DESC')
+    @items = Item.order(id: 'DESC')
   end
 
   def new
