a8eb61d00bd67569f7a4063369531587763d6a36
youliangdao <youliangdaojing5@gmail.com>
2020-11-01 11:37:02 +0900

    コードレビューの修正　itemモデルの単体テストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -65,10 +65,16 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
-      it 'priceが300~9,999,999以外なら登録できない' do
+      it 'priceが299円以下なら登録できない' do
         @item.price = 190
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+
+      it 'priceが10,000,000円以下なら登録できない' do
+        @item.price = 10_000_000_000_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
 
       it 'priceが半角数字以外なら登録できない' do
