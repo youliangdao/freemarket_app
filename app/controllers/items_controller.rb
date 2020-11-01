a28abb250a552614db5dffd0eb09c2a4912070aa
youliangdao <youliangdaojing5@gmail.com>
2020-11-01 10:20:06 +0900

    画像が添付されていない場合の処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,14 +8,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
-    render action: :new and return unless @item.valid?
 
-    redirect_to root_path
+    @item = Item.new(item_params)
+    if @item.valid? && @item.image.attached?
+      redirect_to root_path 
+    else
+      render action: :new
+    end
   end
 
   private
-
   def item_params
     params.require(:item).permit(:image, :title, :detail, :category_id,
                                  :condition_id, :postage_id, :prefecture_id, :delivery_day_id, :price)
