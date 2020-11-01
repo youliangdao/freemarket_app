cee18df6413382a6f432a5c08416ab40a63633a8
youliangdao <youliangdaojing5@gmail.com>
2020-11-01 10:21:19 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,16 +8,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-
     @item = Item.new(item_params)
     if @item.valid? && @item.image.attached?
-      redirect_to root_path 
+      redirect_to root_path
     else
       render action: :new
     end
   end
 
   private
+
   def item_params
     params.require(:item).permit(:image, :title, :detail, :category_id,
                                  :condition_id, :postage_id, :prefecture_id, :delivery_day_id, :price)
