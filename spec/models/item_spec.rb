655c30674ea88911bef86a06ad15aed274ad6cb2
youliangdao <youliangdaojing5@gmail.com>
2020-11-01 11:38:44 +0900

    itemモデルの単体テストコード　修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -71,7 +71,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
 
-      it 'priceが10,000,000円以下なら登録できない' do
+      it 'priceが10,000,000円以上なら登録できない' do
         @item.price = 10_000_000_000_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
