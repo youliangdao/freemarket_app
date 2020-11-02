f66d156af6137d4eafeae53fd5e7cac9fae7ca78
youliangdao <youliangdaojing5@gmail.com>
2020-11-02 21:56:34 +0900

    商品購入機能のバックエンド実装（（購入情報をテーブルに保存）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,18 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:id])
+    @order_address = OrderAddress.new
   end
-  
+
   def create
-    
+    @order_address = OrderAddress.create(order_address)
+    render action: :index and return unless @order_params.valid?
+
+    redirect_to action: :root_path
+  end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :apartment, :phone_number)
   end
 end
