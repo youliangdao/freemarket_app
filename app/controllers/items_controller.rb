1e4e74a55d05fd16ec8d8a5dd525a7feda66f89f
youliangdao <youliangdaojing5@gmail.com>
2020-11-02 15:47:14 +0900

    商品情報編集機能のバックエンド実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.order(id: 'DESC')
   end
@@ -16,15 +18,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    
   end
   
   def update
+    @item.update(item_params)
+    render action: :edit and return unless @item.valid? 
     
+    redirect_to item_path(@item)
   end
 
   private
@@ -33,5 +36,16 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :title, :detail, :category_id,
                                  :condition_id, :postage_id, :prefecture_id, :delivery_day_id, :price)
           .merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    set_item
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
