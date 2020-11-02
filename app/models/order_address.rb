319bfad88aa7d35bc948dd0851f078a9d8bca84c
youliangdao <youliangdaojing5@gmail.com>
2020-11-02 20:46:45 +0900

    Formオブジェクトにデータ保存のための処理を実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,11 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :aprtment, :phone_number
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :apartment, :phone_number
 
-  
+  def save
+    item = Item.find(params[:id])
+    user = item.user
+    order = Order.create(user_id: user.id, item_id: item.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, apartment: apartment, phone_number: phone_number, order_id: item.order_id)
+  end
 end
