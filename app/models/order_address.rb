5dea417089ec04e2530139153820279aad8276f1
youliangdao <youliangdaojing5@gmail.com>
2020-11-02 21:02:20 +0900

    Formオブジェクトにバリデーションを作成

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,6 +2,15 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :address, :apartment, :phone_number
 
+  with_options presence: true do
+    validates :postal_code, format: {with: }
+    validates :city
+    validates :address, format: {with: /\A\d{3}[-]\d{4}\z/}
+    validates :phone_number, /\A\d{1,10}\z
+  end
+
+  validates :prefecture_id, numericality: {other_than: 0}
+
   def save
     item = Item.find(params[:id])
     user = item.user
