6329a96078255f4f30ec1ceeba4c5ee424751ac3
youliangdao <youliangdaojing5@gmail.com>
2020-11-02 16:49:45 +0900

    商品削除機能のバックエンド実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit]
   def index
     @items = Item.order(id: 'DESC')
@@ -31,7 +31,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    
+    @item.destroy
+    redirect_to root_path
   end
 
   private
