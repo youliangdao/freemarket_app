8ac9dc98b11b77249dfbcd88d4058e78d503aca2
youliangdao <youliangdaojing5@gmail.com>
2020-11-03 15:43:20 +0900

    ordersコントローラの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,7 +13,6 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-
       @item = Item.find(params[:item_id])
       render action: :index
     end
